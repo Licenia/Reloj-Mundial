03f3bae3abebd40c33f0fb2a75f326838301b7e0
Licenia Alvear <licialvear@gmail.com>
2025-04-21 15:12:27 -0500

    fix(ui & lógica): reemplazo de input por select para zonas horarias y corrección de lógica de iconos día/noche

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 const cotenedor = document.getElementById("container");
-const inputBox = document.getElementById("input-box");
+const zonaSelect = document.getElementById("zona-select");
 const listContainer = document.getElementById("list-container");
 const message = document.querySelector(".clock-input");
 
@@ -10,50 +10,49 @@ let fechaFormateada;
 let ciudad;
 let ciudadesAgregadas= [];
 
+
 function manejarBusqueda() {
   const nuevaCiudad = addCountry();
   if(nuevaCiudad && !ciudadesAgregadas.includes(nuevaCiudad)){
     ciudadesAgregadas.push(nuevaCiudad);
     obtenerFechayHora(nuevaCiudad);
-
+    
   }
 }
 
 
 function addCountry() {
-  const valorInput = inputBox.value.trim();
-
-  if (valorInput === "" || !/^[a-zA-Z\s\/]+$/.test(valorInput)) {
-    let p = document.createElement("p");
-    p.textContent = "No disponible, Intente nuevamente";
-    message.innerHTML = ""; //para limpiar mensajes anteriores
-    message.appendChild(p);
+  const valorSelect = zonaSelect.value;
+  
+  if (valorSelect === "") {
+    message.innerHTML = "<p>Por favor seleccione una zona horaria</p>";
+    return null;
   }
-  ciudad = valorInput;
-  return ciudad;
-}
-
-setInterval(() =>{
-  ciudadesAgregadas.forEach((ciudad) =>{
-    obtenerFechayHora(ciudad);
-  })
-}, 60000);
-
-
-async function obtenerFechayHora(ciudad) {
-  const url = `https://timeapi.io/api/TimeZone/zone?timeZone=${ciudad}`;
-
-  await fetch(url)
+    ciudad = valorSelect;
+    return ciudad;
+  }
+  
+  setInterval(() =>{
+    ciudadesAgregadas.forEach((ciudad) =>{
+      obtenerFechayHora(ciudad);
+    })
+  }, 60000);
+  
+  
+  async function obtenerFechayHora(ciudad) {
+    const url = `https://timeapi.io/api/TimeZone/zone?timeZone=${ciudad}`;
+    
+    await fetch(url)
     .then((res) => res.json())
     .then((data) => {
       const dateObj = new Date(data.currentLocalTime);
-
+      
       const hora = {
         hour: "2-digit",
         minute: "2-digit",
         hour12: true,
       };
-
+      
       const fecha = {
         day: "2-digit",
         month: "2-digit",
@@ -62,124 +61,123 @@ async function obtenerFechayHora(ciudad) {
 
       horaFormateada = dateObj.toLocaleTimeString("es-Ec", hora);
       fechaFormateada = dateObj.toLocaleDateString("es-Ec", fecha);
-
+      
       mostrarResultados(horaFormateada, ciudad, fechaFormateada);
-
+      
     })
     .catch((err) => console.error(err));
   }
-
-//  despues de 400 ms se ejecutara la funcion de busqueda, esto para evitar hacer muchas peticiones mientras escriba  
-let timeout;
-inputBox.addEventListener("input", () => {
-  clearTimeout(timeout); // sirve cuando el usuario aun no ha terminado de escribir  entonces , se cancela con el setTimeout anterior con clearTimeout(timeout) y creamos uno nuevo
-  timeout = setTimeout(() => {
-    shearch();
-  }, 400)
-});
-
-function shearch() {
-  const url = "https://timeapi.io/api/timezone/availabletimezones";
-  const valorInput = inputBox.value.trim().toLowerCase();
-
-  fetch(url)
+  
+  
+  function shearch() {
+    const url = "https://timeapi.io/api/timezone/availabletimezones";
+    
+    fetch(url)
     .then((res) => res.json())
     .then((timezones) => {
-      const datalist = document.getElementById("buscador");
-      datalist.innerHTML = "";
-
-      // filtrar por coincidencias
-      const zonasFiltradas = timezones.filter((zone) =>
-        zone.toLowerCase().includes(valorInput)
-      );
-
-
-      // Mostrar solo las zonas que coincidan
-      zonasFiltradas.forEach((zone) => {
+      timezones.forEach(zone => {
         const option = document.createElement("option");
         option.value = zone;
-        datalist.appendChild(option);
-      });
+        option.textContent = zone;
+        zonaSelect.appendChild(option);
+      })
     })
     .catch((err) => console.error("Error al obtnener zonas horarias", err));
-}
-
-
-function mostrarResultados(horaFormateada,  ciudad, fechaFormateada){
-  const ciudadExistente = document.querySelector(`[data-ciudad="${ciudad}"]`);
-
-  if(ciudadExistente){
-    ciudadExistente.querySelector(".hora").textContent = horaFormateada;
-    ciudadExistente.querySelector(".fecha").textContent = fechaFormateada;
-    return;
   }
-
-  const li = document.createElement('li');
-  li.setAttribute('data-ciudad', ciudad);
-
-  const hijo1= document.createElement('div');
-  hijo1.setAttribute('class', 'container-img');
-
-  const hijo2 = document.createElement('div');
-  hijo2.setAttribute('class', 'container-hour');
-
-  const hijo3 = document.createElement('div');
-  hijo3.setAttribute('class', 'container-date');
-
-  let icono = document.createElement('img');
-
-  diaNoche(horaFormateada, icono);
-
-  let nieto1 = document.createElement('h2');
-  nieto1.setAttribute('class', 'hora');
-
-  let nieto2 = document .createElement('h2');
-  nieto2.setAttribute('class', 'nombreCiudad')
-
-  let nieto3 = document.createElement('h2');
-  nieto3.setAttribute('class', 'fecha')
   
-  // Asignar contenido
-  nieto1.textContent = horaFormateada;
-  nieto2.textContent = ciudad;
-  nieto3.textContent = fechaFormateada;
-
-  // Armar la estructura
-  hijo1.appendChild(icono);
-  hijo2.appendChild(nieto1);
-  hijo3.appendChild(nieto2);
-  hijo3.appendChild(nieto3);
-
-  li.appendChild(hijo1);
-  li.appendChild(hijo2);
-  li.appendChild(hijo3);
-
-  listContainer.appendChild(li);
-
- 
-}
-
-function diaNoche (horaFormateada, icono){
-  const hora = obtenerHoraEn24(horaFormateada);
-
-  if (hora >= 6 && hora < 18 ) {
-    icono.setAttribute('src', '/img/sol.jpg');
-  }else{
-    icono.setAttribute('src', '/img/luna.jpg');
+  
+  
+  function mostrarResultados(horaFormateada,  ciudad, fechaFormateada){
+    const ciudadExistente = document.querySelector(`[data-ciudad="${ciudad}"]`);
+    
+    if(ciudadExistente){
+      ciudadExistente.querySelector(".hora").textContent = horaFormateada;
+      ciudadExistente.querySelector(".fecha").textContent = fechaFormateada;
+      return;
+    }
+    
+    const li = document.createElement('li');
+    li.setAttribute('data-ciudad', ciudad);
+    
+    const hijo1= document.createElement('div');
+    hijo1.setAttribute('class', 'container-img');
+    
+    const hijo2 = document.createElement('div');
+    hijo2.setAttribute('class', 'container-hour');
+    
+    const hijo3 = document.createElement('div');
+    hijo3.setAttribute('class', 'container-date');
+    
+    let icono = document.createElement('img');
+    
+    diaNoche(horaFormateada, icono);
+    
+    let nieto1 = document.createElement('h2');
+    nieto1.setAttribute('class', 'hora');
+    
+    let nieto2 = document .createElement('h2');
+    nieto2.setAttribute('class', 'nombreCiudad')
+    
+    let nieto3 = document.createElement('h2');
+    nieto3.setAttribute('class', 'fecha')
+    
+    // Asignar contenido
+    nieto1.textContent = horaFormateada;
+    nieto2.textContent = ciudad;
+    nieto3.textContent = fechaFormateada;
+    
+    // Armar la estructura
+    hijo1.appendChild(icono);
+    hijo2.appendChild(nieto1);
+    hijo3.appendChild(nieto2);
+    hijo3.appendChild(nieto3);
+    
+    li.appendChild(hijo1);
+    li.appendChild(hijo2);
+    li.appendChild(hijo3);
+    
+    listContainer.appendChild(li);
+    
+    
   }
-}
-
-function obtenerHoraEn24(horaFormateada) {
-  const [hora, periodo] = horaFormateada.split(" ");
-  let [horas, minutos] = hora.split(":").map(Number); // convierte de string a numero y elimina ":"
-
-  if (periodo.toLowerCase() === "pm" && horas !== 12) {
-    horas += 12;
+  
+  function diaNoche (horaFormateada, icono){
+    const hora = obtenerHoraEn24(horaFormateada);
+    
+    if (hora >= 6 && hora <= 18 ) {
+      icono.setAttribute('src', '/img/sol.jpg');
+    }else{
+      icono.setAttribute('src', '/img/luna.jpg');
+    }
   }
-  if (periodo.toLowerCase() === "am" && horas === 12) {
+
+  function obtenerHoraEn24(horaFormateada) {
+    const [hora, periodoRaw] = horaFormateada.split(" ");
+    const periodo = periodoRaw.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    let [horas, minutos] = hora.split(":").map(Number); // convierte de string a numero y elimina ":"
+    
+    if (periodo === "pm" && horas !== 12) {
+      horas += 12;
+    }
+    if (periodo === "am" && horas === 12) {
     horas = 0;
   }
-
+  
   return horas; 
 }
 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 5000); 
+
+shearch();
