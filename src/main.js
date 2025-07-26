9f799d3e68cd59db11d0da770fd94399889d5d41
Licenia Alvear <licialvear@gmail.com>
2025-07-26 15:04:03 -0500

    feat: formatear nombre de zona y permitir eliminar una card

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {manejarBusqueda, search} from "./script"
+import {manejarBusqueda, search} from "./funciones_reloj"
 
 const d = document,
 $id = d.getElementById("agregarZonaHoraria")
@@ -7,5 +7,13 @@ d.addEventListener("DOMContentLoaded" , () =>{
     if ($id) {
         $id.addEventListener("click", manejarBusqueda)        
     }
+
     search()
+})
+
+d.addEventListener("click", (e) =>{
+     if (e.target.matches(".icono-opciones")) {
+        const $card = e.target.closest(".container-result");
+        $card.remove();
+    }
 })
